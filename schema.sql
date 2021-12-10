17999cc9a770c867567d7746925b6ead7761c953
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-12-11 01:31:14 +0300

    psql

--- schema.sql
+++ schema.sql
@@ -41,3 +41,41 @@ ALTER TABLE
     animals
 add
     owner_id int references owners(id);
+
+-- create vets table
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL,
+    age INT,
+    date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+    vets_id INT,
+    species_id INT,
+    CONSTRAINT vets_id FOREIGN KEY(vets_id) REFERENCES vets(id),
+    CONSTRAINT species_id FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    vets_id INT,
+    animals_id INT,
+    CONSTRAINT vets_id FOREIGN KEY(vets_id) REFERENCES vets(id),
+    CONSTRAINT animals_id FOREIGN KEY(animals_id) REFERENCES animals(id)
+);
+
+ALTER TABLE
+    visits
+ADD
+    visit_date DATE;
+
+ALTER TABLE
+    owners
+ADD
+    email varchar(120);
+
+CREATE INDEX animals_id ON visits(animals_id);
+
+CREATE INDEX visits_id ON visits(vets_id);
+
+CREATE INDEX owners_mail ON owners(email ASC);
