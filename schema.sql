ddb59c84151c712c1ccd8eb93bda52d4f5afb2f8
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-12-10 20:26:58 +0300

    join and add new tables

--- schema.sql
+++ schema.sql
@@ -8,5 +8,36 @@ create table animals (
     weight_kg real,
     primary key(id)
 );
+
 -- add new column species
-alter table animals add species varchar(100);
+alter table
+    animals
+add
+    species varchar(100);
+
+-- creating table owners
+create table owners (
+    id int primary key GENERATED ALWAYS as IDENTITY,
+    full_name varchar(100),
+    age int
+);
+
+-- creating table owners
+create table species (
+    id int primary key GENERATED ALWAYS as IDENTITY,
+    name varchar(100)
+);
+
+-- modifying animals table
+ALTER TABLE
+    animals drop species;
+
+ALTER TABLE
+    animals
+add
+    species_id int references species(id);
+
+ALTER TABLE
+    animals
+add
+    owner_id int references owners(id);
